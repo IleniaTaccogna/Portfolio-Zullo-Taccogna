5ed4d09a04e0f822714866d5a62c44c295823e90
Federico Zullo <fzullo@codearchitects.com>
2025-02-21 10:02:18 +0100

    feat:aggiunta cartelle html,css,sass

--- src/datiProgetti.ts
+++ src/datiProgetti.ts
@@ -6,7 +6,7 @@ export const progetti: Progetti[] = [
         descrizione:
             "La console gestisce prodotti, clienti e ordini in formato JSON. Permette di aggiungere, modificare e rimuovere prodotti, organizzandoli per categoria e con sconti. Gestisce clienti tramite nome, email e codice fiscale/partita IVA, e ordini con calcolo del totale e stato.",
         link: "https://github.com/fzullo/NegozioOnline.git",
-        immagine: "assets/negozioOnline.jpg",
+        immagine:"/assets/negozioOnline.jpg",
     },
     {
         id: 2,
@@ -14,7 +14,7 @@ export const progetti: Progetti[] = [
         descrizione:
             "Il sistema gestisce progetti e task assegnati a team, con ID univoci, stato, priorità e responsabile. Permette di aggiungere progetti, evitare duplicati nei task, modificare lo stato dei task, eliminare progetti e visualizzare progetti e task ad alta priorità per team specifici.",
         link: "https://github.com/pacedom9/gestione-progetti-aziendali",
-        immagine: "assets/progettiAziendali.jpg",
+        immagine: "/assets/progettiAziendali.jpg",
     },
     {
         id: 3,
@@ -22,6 +22,6 @@ export const progetti: Progetti[] = [
         descrizione:
             "Il sistema gestisce atleti e federazioni sportive, con vincoli su iscrizioni e quote annuali. Solo professionisti e semi-professionisti possono iscriversi, e ogni federazione può avere al massimo tre atleti.",
         link: "https://github.com/IleniaTaccogna/Iscrizione-Atleti-Federazioni.git",
-        immagine: "assets/atleti.jpg",
+        immagine: "/assets/atleti.jpg",
     },
 ]; 
